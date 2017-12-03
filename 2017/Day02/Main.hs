63f845aad1d9ddd4ef640a495d02e857fdf18fbe
Brad Parker <hi@bradparker.com>
2017-12-03 22:15:01 +1000

    Day two

---
+++ 2017/Day02/Main.hs
@@ -0,0 +1,55 @@
+module Main where
+
+import Control.Applicative (pure, (<*>), (<|>))
+import Control.Arrow ((&&&))
+import Data.Char (isDigit)
+import Data.Functor.Foldable
+  (ListF (Cons, Nil), para)
+import Data.Maybe (catMaybes, listToMaybe)
+import Text.ParserCombinators.ReadP
+  (munch1, readP_to_S)
+
+parseInt :: String -> Maybe Int
+parseInt =
+  fmap (read . fst) .
+  listToMaybe . readP_to_S (munch1 isDigit)
+
+parseInput :: String -> [[Int]]
+parseInput = map (catMaybes . map parseInt . words) . lines
+
+isMultipleOf :: Integral a => a -> a -> Bool
+isMultipleOf n = (0 ==) . (`mod` n)
+
+isFactorOf :: Integral a => a -> a -> Bool
+isFactorOf n = (0 ==) . (mod n)
+
+divisibleBy :: Integral a => a -> a -> Bool
+divisibleBy n m = isMultipleOf n m || isFactorOf n m
+
+findDivisibleBy :: Integral a => a -> [a] -> Maybe a
+findDivisibleBy n = listToMaybe . filter (divisibleBy n)
+
+findDivisiblePair :: Integral c => [c] -> Maybe (c, c)
+findDivisiblePair = para divisiblePair
+  where
+    divisiblePair Nil = Nothing
+    divisiblePair (Cons n (ns, result)) =
+      ((,) <$> pure n <*> findDivisibleBy n ns) <|> result
+
+orderPair :: Ord a => (a, a) -> (a, a)
+orderPair (a, b) = (max a b, min a b)
+
+dividePair :: Integral c => (c, c) -> c
+dividePair = uncurry div
+
+divideDivisiblePair :: Integral c => [c] -> Maybe c
+divideDivisiblePair =
+  ((dividePair . orderPair) <$>) . findDivisiblePair
+
+checksum :: Integral c => [[c]] -> c
+checksum = sum . catMaybes . map divideDivisiblePair
+
+main :: IO ()
+main = do
+  input <- parseInput <$> getContents
+  print $ checksum input
