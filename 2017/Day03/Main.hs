14fdbe53d2c3800ea765da5612cbc6477240708a
Brad Parker <hi@bradparker.com>
2017-12-06 17:37:19 +1000

    Day three - part two

--- 2017/Day03/Main.hs
+++ 2017/Day03/Main.hs
@@ -1,4 +1,22 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
+
+import Control.Arrow ((>>>))
+import Control.Lens (view, _1)
+import Data.Map
+  (Map (..), empty, insert, lookup, singleton)
+import Data.Maybe (catMaybes, listToMaybe)
+import Prelude hiding (lookup)
+
+type Point = (Int, Int)
+
+type Sum = Int
+
+type StepSum = (Sum, Map Point Sum)
+
+parseInt :: String -> Int
+parseInt = read
 
 right :: Num a => (a, b) -> (a, b)
 right (x, y) = (x + 1, y)
@@ -14,24 +32,63 @@ down (x, y) = (x, y - 1)
 
 block :: Num a => Int -> [(a, a) -> (a, a)]
 block k =
-  replicate (2 * k + 1) right ++
-  replicate (2 * k + 1) up ++
-  replicate (2 * k + 2) left ++
-  replicate (2 * k + 2) down
+  concat
+    [ replicate (2 * k + 1) right
+    , replicate (2 * k + 1) up
+    , replicate (2 * k + 2) left
+    , replicate (2 * k + 2) down
+    ]
 
-spiral :: Int -> [(Int, Int)]
-spiral n = scanl (flip ($)) (0, 0) $ take (n - 1) $ foldMap block [0..]
+spiral :: [Point]
+spiral = scanl (flip ($)) (0, 0) (foldMap block [0 ..])
 
-taxiDistance :: (Int, Int) -> (Int, Int) -> Int
+pointAt :: Int -> Point
+pointAt = (!!) spiral
+
+taxiDistance :: Point -> Point -> Int
 taxiDistance (a, b) (c, d) = abs (a - c) + abs (b - d)
 
 distance :: Int -> Int
-distance = taxiDistance (0, 0) . last . spiral
+distance n = taxiDistance (0, 0) (pointAt (n - 1))
 
-parseInt :: String -> Int
-parseInt = read
+neighbours :: Point -> [Point]
+neighbours p =
+  map
+    ($ p)
+    [ right
+    , up . right
+    , up
+    , up . left
+    , left
+    , down . left
+    , down
+    , down . right
+    ]
 
+pointSum :: Point -> Map Point Int -> Int
+pointSum point sums =
+  sum $
+  catMaybes $ map (flip lookup sums) (neighbours point)
+
+stepSum :: StepSum -> Point -> StepSum
+stepSum (sum, sums) point = (sum', insert point sum' sums)
+  where
+    sum' = pointSum point sums
+
+stepSums :: [StepSum]
+stepSums =
+  scanl stepSum (1, singleton (0, 0) 1) (tail spiral)
+
+firstSumAbove :: Int -> Maybe Int
+firstSumAbove n =
+  view _1 <$>
+  (listToMaybe (filter ((> n) . view _1) stepSums))
+
+-- usage: echo "361527" | stack exec day-03
 main :: IO ()
 main = do
   n <- parseInt <$> getLine
+  putStrLn "Distance from: "
   print $ distance n
+  putStrLn "First sum greater than: "
+  print $ firstSumAbove n
