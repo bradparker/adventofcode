f2040226ebbbba609cd37e0e9aeb350a42629277
Brad Parker <hi@bradparker.com>
2017-12-08 19:57:50 +1000

    Use non-strict zipper for Day 05

--- 2017/Day05/Main.hs
+++ 2017/Day05/Main.hs
@@ -21,26 +21,30 @@ intolerantRight z =
   bool (Just (right z)) Nothing (endp (right z))
 
 move :: Int -> Zipper a -> Maybe (Zipper a)
-move n z =
-  (foldr
-     (<=<)
-     return
-     (replicate
-        (abs n)
-        (bool intolerantRight intolerantLeft (n < 0))))
-    z
+move n =
+  foldr
+    (<=<)
+    return
+    (replicate
+       (abs n)
+       (bool intolerantRight intolerantLeft (n < 0)))
 
-walk :: Int -> Zipper Int -> Int
-walk steps z =
-  case (move n zPosInc) of
-    Nothing -> steps + 1
-    Just zMoved -> walk (steps + 1) zMoved
+replaceA n = replace (n + 1)
+
+replaceB n = replace (bool (n + 1) (n - 1) (n >= 3))
+
+walk ::
+     (Int -> Zipper Int -> Zipper Int) -> Zipper Int -> Int
+walk replacer z = go 0 (Just z)
   where
-    n = cursor z
-    n'' = bool (n + 1) (n - 1) (n >= 3)
-    zPosInc = replace n'' z
+    go acc Nothing = acc
+    go acc (Just z) =
+      go
+        (acc + 1)
+        ((\n -> move n (replacer n z)) =<< cursor z)
 
 main :: IO ()
 main = do
   input <- parseInput <$> getContents
-  print $ walk 0 (fromList input)
+  print $ walk replaceA (fromList input)
+  print $ walk replaceB (fromList input)
